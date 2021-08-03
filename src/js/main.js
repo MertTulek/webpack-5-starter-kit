8ab4c440a497813d4d6e10130ff405d38cdb4566
MertTulek <mert_tulek93@hotmail.com>
2021-08-03 23:02:08 +0300

    Deneme

---
+++ src/js/main.js
@@ -0,0 +1,47 @@
+import Swup from "swup";
+import Swiper from "swiper";
+import "swiper/swiper-bundle.css";
+
+var app = {
+  load() {
+    console.log("load");
+  },
+  resized() {
+    console.log("resized");
+  },
+
+  swiperTest() {
+    const swiper = new Swiper(".swiper-container", {
+      loop: true,
+    });
+  },
+
+  init: function () {
+    app.load();
+    app.swiperTest();
+  },
+};
+
+function docReadied(fn) {
+  window.addEventListener("DOMContentLoaded", fn);
+}
+function docResized(fn) {
+  window.addEventListener("resize", fn);
+}
+docReadied(() => {
+  const swup = new Swup({
+    cache: true,
+  });
+  swup.on("contentReplaced", function () {
+    swup.options.containers.forEach(() => {
+      app.init();
+    });
+  });
+  app.init();
+});
+
+docResized(() => {
+  app.resized();
+});
+
+export default { docResized, docReadied };
